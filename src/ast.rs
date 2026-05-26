b8d831253e95e61bba7e0aaaa095d6870112311a
Simon Désaulniers <sim.desaulniers@gmail.com>
2026-05-26 18:57:03 -0400

    test(ast): string (de)serialization testing

--- src/ast.rs
+++ src/ast.rs
@@ -4,13 +4,13 @@ use std::fmt::Display;
 
 use anyhow::Result;
 
-#[derive(Clone)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum Layout {
     Split { direction: Direction, children: Vec<(u8, Layout)> },
     Leaf  { command: Option<String>, terminal: bool }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum Direction {
     Horizontal,
     Vertical,
@@ -38,6 +38,37 @@ impl FromStr for Direction {
             "t" => Ok(Direction::Tabbed),
             _   => anyhow::bail!("Invalid direction: {}", input)
         }
+    }
+}
+
+#[cfg(test)] mod tests {
+    use super::*;
+
+    #[test]
+    fn direction_from_str_horizontal() {
+        assert_eq!("h".parse::<Direction>().unwrap(), Direction::Horizontal)
+    }
+
+    #[test]
+    fn direction_from_str_vertical() {
+        assert_eq!("v".parse::<Direction>().unwrap(), Direction::Vertical)
+    }
+
+    #[test]
+    fn direction_from_str_tabbed() {
+        assert_eq!("t".parse::<Direction>().unwrap(), Direction::Tabbed)
+    }
+
+    #[test]
+    fn direction_from_str_invalid() {
+        assert!("x".parse::<Direction>().is_err())
+    }
+
+    #[test]
+    fn direction_display() {
+        assert_eq!(Direction::Horizontal.to_string(), "h");
+        assert_eq!(Direction::Vertical.to_string(), "v");
+        assert_eq!(Direction::Tabbed.to_string(), "t");
     }
 }
 
